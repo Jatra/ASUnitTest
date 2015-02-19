c2fabbcba766e551bcf8fa3e5a6c64a661125b17
Tim Russell <tim.j>
2015-02-19 22:16:03 +0000

    Add a Robolectric activity test

---
+++ app/src/test/java/MainActivityTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.util.ActivityController;
+
+import uk.co.jatra.asunittest.MainActivity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MyRobolectricTestRunner.class)
+public class MainActivityTest {
+
+    @Test
+    public void simple() {
+        assertThat(6/2)
+                .isEqualTo(3);
+    }
+
+    @Test
+    public void shouldHaveTitle() {
+        ActivityController<MainActivity> controller = Robolectric.buildActivity(MainActivity.class).create();
+        MainActivity activity = controller.get();
+        String title = activity.getTitle().toString();
+        assertThat(title)
+                .isEqualTo("Hello world!");
+    }
+}
